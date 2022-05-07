5558057146345aeab06a7bada9c39dc5d502ee84
Benito Beceiro <benito_bec@hotmail.com>
2022-05-07 13:59:47 +0100

    added mousemove event to getInTouch

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 "use strict";
-const contacto = document.querySelector("#contact");
-const detail = document
-  .querySelector("#contact")
-  .getElementsByClassName("details");
+const contacto = document.querySelector("#contact, #projects");
+const getInTouch = document.querySelector("#contact h2");
 
-function mouseOver() {
-  detail[0].style.backgroundColor = "#6c757d";
-  detail[0].style.color = "#fff";
-  detail[1].style.backgroundColor = "#6c757d";
-  detail[1].style.color = "#fff";
+function colorChange(e) {
+  getInTouch.style.color = `rgb(${e.offsetX}, ${e.offsetY}, 10)`;
+  // detail[1].style.color = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
 }
 
-function mouseOut() {
-  detail[0].style.backgroundColor = "#fff";
-  detail[0].style.color = "#343a40";
-  detail[1].style.backgroundColor = "#fff";
-  detail[1].style.color = "#343a40";
-}
+console.log(colorChange);
 
-contacto.addEventListener("mouseover", mouseOver);
-contacto.addEventListener("mouseout", mouseOut);
+document.body.addEventListener("mousemove", colorChange);
+
+// function mouseOver() {
+//   detail[0].style.backgroundColor = "#6c757d";
+//   detail[0].style.color = "#fff";
+//   detail[1].style.backgroundColor = "#6c757d";
+//   detail[1].style.color = "#fff";
+// }
+
+// function mouseOut() {
+//   detail[0].style.backgroundColor = "#fff";
+//   detail[0].style.color = "#343a40";
+//   detail[1].style.backgroundColor = "#fff";
+//   detail[1].style.color = "#343a40";
+// }
+
+// contacto.addEventListener("mouseover", mouseOver);
+// contacto.addEventListener("mouseout", mouseOut);
