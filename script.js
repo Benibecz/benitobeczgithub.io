b6782ffc6f92e54e24e040f3aa6ad880dd2d7fdc
Benito Beceiro <benito_bec@hotmail.com>
2022-07-16 10:49:08 +0100

    JS smoothScrolling

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
 "use strict";
+
 const contacto = document.querySelector("#contact");
 const getInTouch = document.querySelector("#contact h2");
 const meetDevBtn = document.querySelector(".meetDev--Btn");
 const meetDevSection = document.querySelector("#meetTheDeveloper");
-const myProjects = document.querySelector(".my--projects");
-const projectsSection = document.querySelector("#projects");
+const navLinks = document.querySelector(".navbar-nav");
 
-const smoothScrolling = function () {
-  const projectsCoords = projectsSection.getBoundingClientRect();
-  console.log(projectsCoords.top);
-  console.log(window.pageYOffset);
-
-  window.scrollTo({
-    left: projectsCoords.left + window.pageXOffset,
-    top: projectsCoords.top + window.pageYOffset,
-    behavior: "smooth",
-  });
+const smoothScrolling = function (e) {
+  // const projectsCoords = projectsSection.getBoundingClientRect();
+  // window.scrollTo({
+  //   left: projectsCoords.left + window.pageXOffset,
+  //   top: projectsCoords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = document.querySelector(e.target.getAttribute("href"));
+    id.scrollIntoView({ behavior: "smooth" });
+  }
+};
+const colorChange = function (e) {
+  getInTouch.style.color = `rgb(${e.offsetX}, ${e.offsetY}, 10)`;
 };
 
-myProjects.addEventListener("click", smoothScrolling);
-
-// -- Modern way of doing smooth scrolling
-// meetDevBtn.addEventListener("click", () =>
-//   meetDevSection.scrollIntoView({ behavior: "smooth" })
-// );
-
-function colorChange(e) {
-  getInTouch.style.color = `rgb(${e.offsetX}, ${e.offsetY}, 10)`;
-  // detail[1].style.color = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
-}
-
+//Event Listeners
 document.body.addEventListener("mousemove", colorChange);
-
-// function mouseOver() {
-//   detail[0].style.backgroundColor = "#6c757d";
-//   detail[0].style.color = "#fff";
-//   detail[1].style.backgroundColor = "#6c757d";
-//   detail[1].style.color = "#fff";
-// }
-
-// function mouseOut() {
-//   detail[0].style.backgroundColor = "#fff";
-//   detail[0].style.color = "#343a40";
-//   detail[1].style.backgroundColor = "#fff";
-//   detail[1].style.color = "#343a40";
-// }
-
-// contacto.addEventListener("mouseover", mouseOver);
-// contacto.addEventListener("mouseout", mouseOut);
+navLinks.addEventListener("click", smoothScrolling);
